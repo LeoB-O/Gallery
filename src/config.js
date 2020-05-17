bb3320311e2b64ddb59b62040b689c09613e74d0
leob_o <liubobotb@126.com>
2020-05-17 18:45:41 +0800

    fix: request url

--- src/config.js
+++ src/config.js
@@ -4,18 +4,18 @@
  * @Author: Leo
  * @Date: 2020-05-10 19:36:45
  * @LastEditors: Leo
- * @LastEditTime: 2020-05-11 18:56:29
+ * @LastEditTime: 2020-05-17 18:44:46
  */
 const config = {
   production: {
     baseUrl: "https://api.leobob.cn/gallery",
     staticUrl: "http://www.leobob.cn/gallery",
-    removeUrl: "https://api.leobob.cn/remove"
+    removeUrl: "https://api.leobob.cn/gallery/remove"
   },
   development: {
     baseUrl: "http://localhost:4001/gallery",
     staticUrl: "http://localhost:4001/gallery/gallery",
-    removeUrl: "http://localhost:4001/remove"
+    removeUrl: "http://localhost:4001/gallery/remove"
   },
 };
 
